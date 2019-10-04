274a6172c2a89b21c4df2f083b068c8c81366bff
alphasr <nilaybaranwal@gmail.com>
2019-10-04 17:00:12 +0200

    test the findPrimes function

--- src/main/scala/hw1b/Main.scala
+++ src/main/scala/hw1b/Main.scala
@@ -1,20 +1,39 @@
-package hw1
+package hw1b
 
-object Main {
+import scala.collection.immutable._
+import scala.collection.{immutable, mutable}
 
-  /* Exercise 1 */
-  def levensteinDistance(a: String, b: String): Int = ???
+object Main extends App {
+    /* Exercise 1 */
+    def levensteinDistance(a: String, b: String): Int = ???
 
-  /* Exercise 2 */
-  abstract class Lexeme
-  case class Operator(symbol: String) extends Lexeme
-  case class Operand(value: Int) extends Lexeme
+    /* Exercise 2 */
+    abstract class Lexeme
+    case class Operator(symbol: String) extends Lexeme
+    case class Operand(value: Int) extends Lexeme
 
-  def infixToRPN(expression: List[Lexeme]): List[Lexeme] = ???
+    def infixToRPN(expression: List[Lexeme]): List[Lexeme] = ???
 
-  /* Exercise 3 */
-  def findPrimes(n: Int): Set[Int] = ???
+    /* Exercise 3 */
+    def findPrimes(n: Int): Set[Int] = {
 
-  /* Exercise 4 */
-   def convertFromRomanNum(n: String): Int = ???
-}
+        if(n==1){var s : Set[Int] = Set()
+        return s}
+
+        val primes = mutable.ArrayBuffer[Int](2)
+
+        def isPrime(i: Int): Boolean =
+            primes.takeWhile(_ <= math.sqrt(i).toInt).forall(i % _ != 0)
+
+        for (k <- 3 until n)
+            if (isPrime(k))
+                primes += k
+         primes.toSet
+        val ss = collection.immutable.SortedSet[Int]() ++ primes
+
+        ss
+    }
+
+    /* Exercise 4 */
+    def convertFromRomanNum(n: String): Int = ???
+  }
--- src/test/scala/hw1b/InfixToRPNTest.scala
+++ src/test/scala/hw1b/InfixToRPNTest.scala
@@ -1,4 +1,4 @@
-package hw1
+package hw1b
 
 import org.scalatest.FunSuite
 
